f85e71da05021205e1a6a08ae30d35d08ee02533
vergik <mihailovpeter@gmail.com>
2020-11-01 02:21:51 +0300

    -

--- src/Race.java
+++ src/Race.java
@@ -1,11 +1,28 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Race {
     private ArrayList<Stage> stages;
+    private boolean somebodyFinished = false;
+    private Lock somebodyFinishedLock = new ReentrantLock();
 
     public ArrayList<Stage> getStages() {
         return stages;
+    }
+
+    public void iAmFinish(Car car) {
+        somebodyFinishedLock.lock();
+
+        if (!somebodyFinished) {
+            somebodyFinished = true;
+            System.out.println("\n" + car.getName() + " WIN\n");
+        } else {
+            System.out.println(car.getName() + " завершил гонку");
+        }
+
+        somebodyFinishedLock.unlock();
     }
 
     public Race(Stage... stages) {
